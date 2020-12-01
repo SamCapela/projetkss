736086fda98caac38993eaef92e3af9ab7caadee
Kellian Jonville <kellianjonville@imac-de-kellian.local>
2020-12-01 13:16:11 +0000

    Modification connexion

--- application/views/auth/connexion.php
+++ application/views/auth/connexion.php
@@ -2,18 +2,63 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $this->load->view('header');
 ?>
-<div class="container text-center">
-	<form method="POST">
-		<label>Email : 
-			<input class="form-control" type="email" placeholder="email@email.com" name="connexion[email]"/>
-		</label>
-		<label>Mot de passe : 
-			<input class="form-control" type="password" placeholder="*****" name="connexion[password]"/>
-		</label>
-		<button class="btn-primary">Connexion</button>
-	</form>	
-</div>
 
+<div class="container">
+
+	<!-- Outer Row -->
+	<div class="row justify-content-center">
+
+		<div class="col-xl-10 col-lg-12 col-md-9">
+
+			<div class="card o-hidden border-0 shadow-lg my-5">
+				<div class="card-body p-0">
+					<!-- Nested Row within Card Body -->
+					<div class="row">
+						<div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+						<div class="col-lg-6">
+							<div class="p-5">
+								<div class="text-center">
+									<h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+								</div>
+								<form class="user"  method="POST">
+									<div class="form-group">
+										<input type="email" name="connexion[email]" class="form-control form-control-user"
+											id="exampleInputEmail" aria-describedby="emailHelp"
+											placeholder="Enter Email Address...">
+									</div>
+									<div class="form-group">
+										<input type="password" name="connexion[password]" class="form-control form-control-user"
+											id="exampleInputPassword" placeholder="Password">
+									</div>
+									<button type="submit"  class="btn btn-primary btn-user btn-block">
+										Login
+									</button>
+									<hr>
+									<a href="index.html" class="btn btn-google btn-user btn-block">
+										<i class="fab fa-google fa-fw"></i> Login with Google
+									</a>
+									<a href="index.html" class="btn btn-facebook btn-user btn-block">
+										<i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+									</a>
+								</form>
+								<hr>
+								<div class="text-center">
+									<a class="small" href="forgot-password.html">Forgot Password?</a>
+								</div>
+								<div class="text-center">
+									<a class="small" href="register.html">Create an Account!</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $this->load->view('footer');
