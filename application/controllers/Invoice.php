ec6a30d7bac5ce001fc170d8767ebbde6f5cf81d
Kellian Jonville <kellianjonville@imac-de-kellian.local>
2020-12-02 14:34:29 +0000

    modifcation invoice

--- application/controllers/Invoice.php
+++ application/controllers/Invoice.php
@@ -66,7 +66,9 @@ class Invoice extends CI_Controller {
 					'price' => $val['price']
 				);
 				$ci->InvoiceModel->addInvoiceDetail($data_invoice_detail);
-				$ci->load->view('errors/invoice_success');
+				$base_url = base_url();
+				$redirect = $base_url.'Invoice';
+				header('Location: '.$redirect);
 				// push
 
 			}
